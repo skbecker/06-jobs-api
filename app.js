518cb2f42bbb15cbd35e6c19890b76e10df09733
skbecker <becker.summer@outlook.com>
2023-01-29 19:39:26 -0500

    'week10'

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
 require('express-async-errors');
+
+//extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 const express = require('express');
 const app = express();
 
@@ -14,9 +21,20 @@ const eventsRouter = require('./routes/events')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
-app.use(express.json());
-// extra packages
+app.set('trust proxy', 1)
 
+app.use(rateLimiter({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+}))
+app.use(express.json());
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
+app.get('/', (req,res)=> {
+  res.send('Events API')
+})
 // routes
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/events', authenticateUser, eventsRouter)
